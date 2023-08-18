5aa8b8dfe400538bea0d805851760fab3a0f8fd7
Tmy2017 <tommykao2013@gmail.com>
2023-08-18 14:03:16 +0000

    APP_DOMAIN = url WOULD change if using gitpod with PREBUILD, hence must generate at REAL workspace starts     #   TOOD: this will cause .env file keeps having new lines of APP_DOMAIN, but now I could not find "ONE TIME" but     #   not at init phase solution

--- gitpod-init.sh
+++ gitpod-init.sh
@@ -2,7 +2,6 @@
 
 ln -s "$(which ruby)" "$(rvm gemdir)/bin/ruby"
 cp .env_sample .env
-echo "APP_DOMAIN=\"$(gp url 3000 | cut -f 3 -d /)\"" >> .env
 echo 'APP_PROTOCOL="https://"' >> .env
 gem install solargraph
 gem install foreman
